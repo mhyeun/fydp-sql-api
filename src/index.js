54b5f4136fd9f2dc02a03a92a864ecdc7aa54bca
Matthew Hyeun <jhyeun@uwaterloo.ca>
2023-02-10 13:27:25 -0500

    Remove unnecessary comments

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ require('dotenv').config()
 const sql = require('mssql');
 
 const config = {
-    user: process.env.DB_USER, // better stored in an app setting such as process.env.DB_USER
-    password: process.env.DB_PASSWORD, // better stored in an app setting such as process.env.DB_PASSWORD
-    server: process.env.DB_SERVER, // better stored in an app setting such as process.env.DB_SERVER
-    port: parseInt(process.env.DB_PORT, 10), // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: process.env.DB_NAME, // better stored in an app setting such as process.env.DB_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    port: parseInt(process.env.DB_PORT, 10),
+    database: process.env.DB_NAME,
     authentication: {
         type: 'default'
     },
