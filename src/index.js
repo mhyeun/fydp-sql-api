68c498843d37a75004e8a7b6a9e126c62a43bcf5
Matthew Hyeun <jhyeun@uwaterloo.ca>
2023-02-10 13:15:38 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// Configure express
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Configure env variables
+require('dotenv').config()
+
+// Connect to db
+const sql = require('mssql');
+
+const config = {
+    user: process.env.DB_USER, // better stored in an app setting such as process.env.DB_USER
+    password: process.env.DB_PASSWORD, // better stored in an app setting such as process.env.DB_PASSWORD
+    server: process.env.DB_SERVER, // better stored in an app setting such as process.env.DB_SERVER
+    port: parseInt(process.env.DB_PORT, 10), // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: process.env.DB_NAME, // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
+
+const connectAndQuery = async () => {
+    try {
+        console.log("Attemping to connect to db...")
+        var poolConnection = await sql.connect(config);
+        console.log("Connected!")         
+        
+        poolConnection.close();
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+connectAndQuery()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
